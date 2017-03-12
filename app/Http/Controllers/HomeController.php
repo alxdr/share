257bb41e31cd3fd3d554374f7c157c3016af463e
Shalom Quek <shalomquek@gmail.com>
2017-03-12 18:27:39 +0800

    Home Screen Template - Able to see User's Items

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Connection as Conn;
+use \PDO as PDO;
 
 class HomeController extends Controller
 {
@@ -11,10 +14,15 @@ class HomeController extends Controller
      *
      * @return void
      */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
+     private $pdo;
+
+     public function __construct() {
+     	try {
+ 	    $this->pdo = Conn::connect();
+     	} catch (\PDOException $err) {
+ 	    echo $err.getMessage();
+     	}
+     }
 
     /**
      * Show the application dashboard.
@@ -23,6 +31,24 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $user_id = Auth::id();
+        echo $user_id;
+
+        $loans_result = $this->pdo->query("SELECT i.item_id, i.description, i.availability, l.loan_date, l.return_date
+            FROM Items i LEFT OUTER JOIN Loan_history l ON i.item_id = l.item_id WHERE i.owner = $user_id ORDER BY i.item_id");
+    	$loans_table = [];
+    	while ($row = $loans_result->fetch(PDO::FETCH_ASSOC)) {
+    	    $loans_table[] = $row;
+    	}
+
+
+        $items_result = $this->pdo->query("SELECT i.item_id, i.description, i.availability, b.bid_value
+            FROM Items i LEFT OUTER JOIN Bid_history b ON i.item_id = b.item_id WHERE i.owner = $user_id ORDER BY i.item_id");
+    	$items_table = [];
+    	while ($row = $items_result->fetch(PDO::FETCH_ASSOC)) {
+    	    $items_table[] = $row;
+    	}
+
+        return view('home')->with('items_table', $items_table)->with('loans_table', $loans_table);
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,9 +6,72 @@
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
                 <div class="panel-heading">Dashboard</div>
-
                 <div class="panel-body">
                     You are logged in!
+                </div>
+                <div class="panel-heading">Items You Bid For
+
+
+
+
+                </div>
+                <div class="panel-heading">Your Items on Loan
+                    <div class="panel-body">
+                        <div class="table-responsive" align="center">
+                            <table class="table-striped table-bordered table-hover">
+                                <thead>
+                                <tr>
+                                <th style="padding:5px;"> Item ID </th>
+                                <th style="padding:5px;"> Item Description </th>
+                                <th style="padding:5px;"> Item Status </th>
+                                <th style="padding:5px;"> Loan Date </th>
+                                <th style="padding:5px;"> Return Date </th>
+                                </tr>
+                                </thead>
+                                <tbody>
+
+                                </tbody>
+                                </table>
+                            </div>
+                        </div>
+                </div>
+
+                <div class="panel-heading">Your For Bidding
+                    <div class="panel-body">
+                        <div class="table-responsive" align="center">
+                            <table class="table-striped table-bordered table-hover">
+                                <thead>
+                                <tr>
+                                <th style="padding:5px;"> Item ID </th>
+                                <th style="padding:5px;"> Item Description </th>
+                                <th style="padding:5px;"> Item Status </th>
+                                <th style="padding:5px;"> Current Highest Bid </th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                @foreach ($items_table as $row)
+                                	<tr>
+                                	@foreach ($row as $col)
+                                		@if ($loop->iteration > 4)
+                                		@elseif ($loop->iteration == 3)
+                                			@if ($col == 1)
+                                				<td> Out For Bid </td>
+                                			@else
+                                				<td> On Loan </td>
+                                			@endif
+                                		@elseif($loop->iteration == 1)
+                                            <td><a href="update_item?item_id={{$col}}">{{ $col }}</a></td>
+                                        @else
+                                			<td> {{ $col }} </td>
+                                		@endif
+                                	@endforeach
+                                	</tr>
+                                @endforeach
+                                </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
